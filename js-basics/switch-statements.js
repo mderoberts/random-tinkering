f0b7095247c683a079be95595bece737cf8d3037
mderoberts <mderoberts@users.noreply.github.com>
2017-11-07 15:36:19 -0500

    switch statement review

---
+++ js-basics/switch-statements.js
@@ -0,0 +1,12 @@
+let eatFood = "Pizza";
+
+switch (eatFood) {
+  case "Cheeseburger": alert("You want to eat a cheeseburger!");
+    break;
+  case "Pizza": alert("You want to eat pizza!");
+    break;
+  case "Sushi": alert("You want to eat sushi!");
+    break;
+  default: alert("Nothing? Guess you're not hungry");
+    break;
+}
