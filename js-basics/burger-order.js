877ecc6c8482874d067507a07507f037d94dc35b
mderoberts <mderoberts@users.noreply.github.com>
2017-11-07 16:22:34 -0500

    cost with tax

--- js-basics/burger-order.js
+++ js-basics/burger-order.js
@@ -1,9 +1,26 @@
 let orderCount = 0;
 
-const orderBurger = (burgerTemp, toppings, breadType) => {
+const orderBurger = (burgerTemp, breadType) => {
     orderCount++;
-    console.log(`You want a burger cooked ${burgerTemp} with ${toppings} on ${breadType}.`)
+    console.log(`You want a burger cooked ${burgerTemp} on ${breadType}.`)
 };
 
-console.log(orderBurger("medium", "lettuce, tomato, onion", "brioche"));
+console.log(orderBurger("medium", "brioche"));
+console.log(orderBurger("rare", "sesame"));
+console.log(orderBurger("well done", "lettuce wrap"));
+console.log(orderBurger("medium-well", "kaiser"));
 console.log(orderCount);
+
+const getSubtotal = (itemCount) => {
+    return itemCount * 10; 
+};
+
+const getTax = () => {
+    return getSubtotal(orderCount) * 0.06;
+};
+
+const totalCost = () => {
+    return getSubtotal(orderCount) + getTax();
+};
+
+console.log(totalCost());
