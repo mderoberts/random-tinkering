c1fcb2f44835eb7fc56115616885badcf61e5a98
mderoberts <mderoberts@users.noreply.github.com>
2017-11-07 11:22:00 -0500

    function expression

--- js-basics/function-expressions-declarations.js
+++ js-basics/function-expressions-declarations.js
@@ -1,4 +1,16 @@
+// Function declaration
 function isGreaterThan(numberOne, numberTwo) {
+  if (numberOne > numberTwo) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isGreaterThan(10, 5));
+
+// Function expression w/ arrow function
+const isGreaterThan = (numberOne, numberTwo) => {
   if (numberOne > numberTwo) {
     return true;
   } else {
