0481a5f0acd6939bbff089e243a1c73e65a72978
mderoberts <mderoberts@users.noreply.github.com>
2017-11-08 15:45:19 -0500

    array review

---
+++ js-basics/array-fun.js
@@ -0,0 +1,22 @@
+//Logging array, changing index value
+let myArray = ['String 1', 'String 2', 'String 3'];
+
+console.log(myArray);
+
+let listItem = myArray[0];
+console.log(listItem);
+console.log(myArray[2]);
+console.log(myArray[3]);
+
+myArray[1] = 'Replacement string';
+
+// Index of word
+let word = "Word";
+
+console.log(word[2]);
+
+//Checking length
+
+let myArray2 = ['String 1', true, 'Another string'];
+typeof myArray2;
+myArray2.length;
