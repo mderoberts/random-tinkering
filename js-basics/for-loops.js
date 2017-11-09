e13477c3826c47e43290c739e9d94e6cc326c88a
mderoberts <mderoberts@users.noreply.github.com>
2017-11-09 12:35:24 -0500

    backward for loop

--- js-basics/for-loops.js
+++ js-basics/for-loops.js
@@ -1,1 +1,7 @@
+// Backward for loop
 
+let favoriteSpots = ['Spot 1', 'Spot 2', 'Spot 3'];
+
+for (let favoriteSpotIndex = favoriteSpots.length - 1; favoriteSpotIndex >= 0; favoriteSpotIndex--) {
+  console.log('I would love to go to ' +  favoriteSpots[favoriteSpotIndex]);
+}
