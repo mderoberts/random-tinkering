574c558b25d5453f53735c33c96e062aef2d0ac0
mderoberts <mderoberts@users.noreply.github.com>
2017-11-10 13:09:17 -0500

    forEach with arrow

--- js-basics/iterators.js
+++ js-basics/iterators.js
@@ -5,3 +5,9 @@ let things = ['thing 1', 'thing 2', 'thing 2', 'thing 2'];
 things.forEach(function(thing) {
     console.log(`I want to buy a ${thing}.`);
 });
+
+// forEach w/ =>
+
+let things = ['thing 1', 'thing 2', 'thing 2', 'thing 2'];
+
+things.forEach(thing => console.log(`I want to buy a ${thing}.`));
