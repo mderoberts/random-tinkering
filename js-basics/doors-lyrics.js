8fef07f8a220246faf37ab908924e15ce6290c24
mderoberts <mderoberts@users.noreply.github.com>
2017-11-10 16:48:28 -0500

    verse 1

--- js-basics/doors-lyrics.js
+++ js-basics/doors-lyrics.js
@@ -1,1 +1,15 @@
+let message = ['Hello', 'I love you', "won't you", 'tell me', ]
+message.push('your name?');
 
+// message.forEach(phrase => console.log(phrase));
+
+let message2 = [message[0], message[1], 'let me jump in your game?'];
+
+// message2.forEach(phrase => console.log(phrase));
+
+const verse1 = () => {
+	message.forEach(phrase => console.log(phrase));
+	message2.forEach(phrase => console.log(phrase));
+};
+
+verse1();
