f4df9e13be763b3ffbbd97999779ea8e4c371943
mderoberts <mderoberts@users.noreply.github.com>
2017-11-11 22:11:57 -0500

    forEach return undefined

--- js-basics/iterators.js
+++ js-basics/iterators.js
@@ -11,3 +11,14 @@ things.forEach(function(thing) {
 let things = ['thing 1', 'thing 2', 'thing 2', 'thing 2'];
 
 things.forEach(thing => console.log(`I want to buy a ${thing}.`));
+
+// forEach to all caps, return undefined
+
+
+let fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+
+let newFruits = fruits.forEach(fruit => console.log(fruit.toUpperCase()));
+
+console.log(newFruits);
+
