bb739b13947c5992de4a7a4abd6a5f936c612acb
mderoberts <mderoberts@users.noreply.github.com>
2017-11-13 17:39:26 -0500

    Update objects.js

--- js-basics/objects.js
+++ js-basics/objects.js
@@ -6,4 +6,10 @@ let myObject = {
 myObject.prop3 = ['thing 1', 'thing 2', 'thing 3'];
 myObject.prop3.push('thing 4', 'thing 5');
 
+myObject.myMethod = () => {
+	return 'Success!';
+};
+
+
 console.log(myObject);
+console.log(myObject.myMethod());
