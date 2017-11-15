f78258f69ed1b1e784af66066092365c0b1e8fd7
mderoberts <mderoberts@users.noreply.github.com>
2017-11-15 13:44:14 -0500

    Update objects.js

--- js-basics/objects.js
+++ js-basics/objects.js
@@ -1,10 +1,10 @@
-let something = 1;
-let message = 'nothing';
+let something = '1';
+let message = 'prop2';
 
 let myObject = {
   prop1: 'String',
   prop2: 23
-}
+};
 
 myObject.prop3 = ['thing 1', 'thing 2', 'thing 3'];
 myObject.prop3.push('thing 4', 'thing 5');
@@ -17,15 +17,18 @@ myObject.myMethod2 = function() {
 	return `I like ${this.prop3[2]}!`;
 };
 
-if (something = 1) {
-	message = myObject.prop3[4];
-} else if (something = 3) {
-	message = myObject.prop3[0];
-} else {
-	message = message;
-}
+something = 1;
+
+if (typeof something === 'number') {
+    //   message = myObject.prop3[4];
+    message = 'prop1';
+} 
 	
 console.log(myObject[message]);
+console.log(typeof something);
+// console.log(myObject.prop3[4]);
+console.log(myObject.prop3);
+
 
 
 
